bd4ff7e0f2b6a58a7ac43a2d5f0f9904409cf6ae
ropok <jalerse@gmail.com>
2016-10-04 15:40:24 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Ecoteer - Mobile Apps for Eco Tourism
+  This Mobile apps made for Fuze Ecoteer that have purpose for helping tourism to understand how to do ecotravel and explore any kind of local culture especially at Pulau Perhentian Kecil, Malaysia
+
+## What we need
+1. Anykind of feedback for this apps
+2. Volunteering to help the design (Front-End), since the developer just focused on the Back-end
+If necessary can be directly sent me email to jalerse@gmail.com
+
+## Mobile Apps Prototype
+![alt tag](https://raw.githubusercontent.com/ropok/imateGest/master/Prototype%20Ecoteer%20Apps%20View%202.jpg )
+## Prototype Detail
+###**1. Turtle Project**
+  To help user understanding about turtles (check the detail here : [Submit Turtle Photos](https://www.facebook.com/perhentianturtleproject/photos/a.1504889479793206.1073741828.1504761523139335/1522655808016573/?type=3&theater))
+  ![alt tag](https://raw.githubusercontent.com/ropok/imateGest/master/Ecoteer%20Apps%20pg1.jpg)
+###**2. Report any photos for eco awareness**
+  Snap a photo and send it directly to any social media
+  ![alt tag](https://raw.githubusercontent.com/ropok/imateGest/master/Ecoteer%20Apps%20pg2.jpg)
+  ![alt tag](https://raw.githubusercontent.com/ropok/imateGest/master/Ecoteer%20Apps%20pg6.jpg)
+###**3. Maps with Eco Travel Icons (Diving sites, Snorkle sites, etc)**
+  Help the user to knowing any interesting location
+  Clickable icon to see the details (directly open website or using local database)
+  ![alt tag](https://raw.githubusercontent.com/ropok/imateGest/master/Ecoteer%20Apps%20pg3.jpg)
+###**4. Explore Perhentian**
+  Explore any local culture like food, Malay Dinner, Basic Malay Dictionary, etc(features shown on image)
+  ![alt tag](https://raw.githubusercontent.com/ropok/imateGest/master/Ecoteer%20Apps%20pg4.jpg)
+###**5. Pop-up Information**
+  Information to encourage the user to be Eco Tourism by understanding the [Marine Park Rule](http://greenfins.net/best-dive-practices)
+  ![alt tag](https://raw.githubusercontent.com/ropok/imateGest/master/Ecoteer%20Apps%20pg5.jpg)
+## License
+MIT License
---
+++ Sample/MainActivity.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using com.refractored;
+using Java.Interop;
+using Java.Lang;
+using Fragment = Android.Support.V4.App.Fragment;
+using FragmentManager = Android.Support.V4.App.FragmentManager;
+using String = Java.Lang.String;
+
+namespace Sample
+{
+    [Activity(Label = "Fuze Ecoteer", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : BaseActivity, IOnTabReselectedListener, ViewPager.IOnPageChangeListener
+    {
+        private MyPagerAdapter adapter;
+        private int count = 1;
+        private int currentColor;
+        private Drawable oldBackground;
+        private ViewPager pager;
+        private PagerSlidingTabStrip tabs;
+
+        protected override int LayoutResource
+        {
+            get { return Resource.Layout.activity_main; }
+        }
+
+        public void OnPageScrollStateChanged(int state)
+        {
+            Console.WriteLine("Page scroll state changed: " + state);
+        }
+
+        public void OnPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+        {
+            Console.WriteLine("Page Scrolled");
+        }
+
+        public void OnPageSelected(int position)
+        {
+            Console.WriteLine("Page selected: " + position);
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            adapter = new MyPagerAdapter(SupportFragmentManager);
+            pager = FindViewById<ViewPager>(Resource.Id.pager);
+            tabs = FindViewById<PagerSlidingTabStrip>(Resource.Id.tabs);
+            pager.Adapter = adapter;
+            tabs.SetViewPager(pager); // Menu Tab - Action bar
+
+            var pageMargin = (int) TypedValue.ApplyDimension(ComplexUnitType.Dip, 4, Resources.DisplayMetrics);
+            pager.PageMargin = pageMargin;
+            pager.CurrentItem = 0;
+            tabs.OnTabReselectedListener = this;
+            tabs.OnPageChangeListener = this;
+
+            SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+            SupportActionBar.SetHomeButtonEnabled(false);
+
+            ChangeColor(Resources.GetColor(Resource.Color.blue));
+        }
+
+        private void ChangeColor(Color newColor)
+        {
+            tabs.SetBackgroundColor(newColor);
+
+            // change ActionBar color just if an ActionBar is available
+            Drawable colorDrawable = new ColorDrawable(newColor);
+            Drawable bottomDrawable = new ColorDrawable(Resources.GetColor(Android.Resource.Color.Transparent));
+            var ld = new LayerDrawable(new[] {colorDrawable, bottomDrawable});
+            if (oldBackground == null)
+            {
+                SupportActionBar.SetBackgroundDrawable(ld);
+            }
+            else
+            {
+                var td = new TransitionDrawable(new[] {oldBackground, ld});
+                SupportActionBar.SetBackgroundDrawable(td);
+                td.StartTransition(200);
+            }
+
+            oldBackground = ld;
+            currentColor = newColor;
+        }
+
+        [Export("onColorClicked")]
+        public void OnColorClicked(View v)
+        {
+            var color = Color.ParseColor(v.Tag.ToString());
+            ChangeColor(color);
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+            outState.PutInt("currentColor", currentColor);
+        }
+
+        protected override void OnRestoreInstanceState(Bundle savedInstanceState)
+        {
+            base.OnRestoreInstanceState(savedInstanceState);
+            currentColor = savedInstanceState.GetInt("currentColor");
+            ChangeColor(new Color(currentColor));
+        }
+
+        #region IOnTabReselectedListener implementation
+
+        public void OnTabReselected(int position)
+        {
+            Toast.MakeText(this, "Tab reselected: " + position, ToastLength.Short).Show();
+        }
+
+        #endregion
+
+        #region menu
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            var inflater = MenuInflater;
+            inflater.Inflate(Resource.Menu.main, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            // Handle presses on the action bar items
+            switch (item.ItemId)
+            {
+
+                case Resource.Id.action_icons:
+
+                    var intent = new Intent(this, typeof(SecondActivity));
+                    StartActivity(intent);
+                    return true;
+
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
+        }
+
+        #endregion
+    }
+
+    public class MyPagerAdapter : FragmentPagerAdapter
+    {
+        private readonly string[] Titles =
+        {
+                "Turtle Project", "Report", "Map", "Explore"
+        };
+
+        public MyPagerAdapter(FragmentManager fm) : base(fm)
+        {
+        }
+
+        public override ICharSequence GetPageTitleFormatted(int position)
+        {
+            return new String(Titles[position]);
+        }
+
+        #region implemented abstract members of PagerAdapter
+
+        public override int Count
+        {
+            get { return Titles.Length; }
+        }
+
+        #endregion
+
+        #region implemented abstract members of FragmentPagerAdapter
+
+        public override Fragment GetItem(int position)
+        {
+            return SuperAwesomeCardFragment.NewInstance(position);
+        }
+
+        #endregion
+    }
+}
